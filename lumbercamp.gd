8b1f75d00b9d4b1198f0191389f183bff84547c1
Michael Kroell <kroellmichael@web.de>
2023-07-26 18:19:49 +0200

    Project init

---
+++ lumbercamp.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var lumbercampDialg = $Window
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_static_body_2d_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
